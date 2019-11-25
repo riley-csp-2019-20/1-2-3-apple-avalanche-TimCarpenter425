d6d1f1aa436c079633e89a3fc6a079bb84aae353
TimCarpenter425 <55500718+TimCarpenter425@users.noreply.github.com>
2019-11-25 10:55:11 -0500

    Update 123timcarpenter.py

--- 123timcarpenter.py
+++ 123timcarpenter.py
@@ -3,272 +3,202 @@ import turtle as trtl
 import random as rndm
 
 apple_image = "apple.gif" # Store the file name of your shape
+ground_height = -200
+apple_letter_x_offset = -25
+apple_letter_y_offset = -50
 
-letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+screen_width = 400
+screen_height = 400
+letter_list = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", 
+"K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
+current_letters = []
+apple_list = []
+number_of_apples = 5
+
+current_letter = "A"
 
 wn = trtl.Screen()
+wn.setup(width=1.0, height=1.0)
 wn.addshape(apple_image) # Make the screen aware of the new file
-wn.setup(width = 1.0, height = 1.0)
+
 wn.bgpic("tree.gif")
+# apple = trtl.Turtle()
+# apple.penup()
+wn.tracer(False)
 
-apple = trtl.Turtle()
-apple.penup()
-apple.speed(0)
-
-letter = trtl.Turtle()
-letter.ht()
-letter.speed(0)
-letter.penup()
-letter.pencolor("white")
-
+def reset_apple(active_apple):
+  global current_letter
+  length_of_list = len(letter_list)
+  if (length_of_list != 0):
+    index = rndm.randint(0, length_of_list)
+    current_letter = letter_list.pop(index)
+    new_apple_x = rndm.randint(-160, 150)
+    new_apple_y = rndm.randint(-30, 150)
+    active_apple.goto(new_apple_x, new_apple_y)
+    draw_apple(active_apple, current_letter)
+    current_letters.append(current_letter)
 # given a turtle, set that turtle to be shaped by the image file
-def start_gravity_a():
-    global g
-    global new_letter
-    if(new_letter == "A"):
-        g = 1
-        gravity()
-def start_gravity_b():
-    global g
-    global new_letter
-    if(new_letter == "B"):
-        g = 1
-        gravity()
-def start_gravity_c():
-    global g
-    global new_letter
-    if(new_letter == "C"):
-        g = 1
-        gravity()
-def start_gravity_d():
-    global g
-    global new_letter
-    if(new_letter == "D"):
-        g = 1
-        gravity()
-def start_gravity_e():
-    global g
-    global new_letter
-    if(new_letter == "E"):
-        g = 1
-        gravity()
-def start_gravity_f():
-    global g
-    global new_letter
-    if(new_letter == "F"):
-        g = 1
-        gravity()
-def start_gravity_g():
-    global g
-    global new_letter
-    if(new_letter == "G"):
-        g = 1
-        gravity()
-def start_gravity_h():
-    global g
-    global new_letter
-    if(new_letter == "H"):
-        g = 1
-        gravity()
-def start_gravity_i():
-    global g
-    global new_letter
-    if(new_letter == "I"):
-        g = 1
-        gravity()
-def start_gravity_j():
-    global g
-    global new_letter
-    if(new_letter == "J"):
-        g = 1
-        gravity()
-def start_gravity_k():
-    global g
-    global new_letter
-    if(new_letter == "K"):
-        g = 1
-        gravity()
-def start_gravity_l():
-    global g
-    global new_letter
-    if(new_letter == "L"):
-        g = 1
-        gravity()
-def start_gravity_m():
-    global g
-    global new_letter
-    if(new_letter == "M"):
-        g = 1
-        gravity()
-def start_gravity_n():
-    global g
-    global new_letter
-    if(new_letter == "N"):
-        g = 1
-        gravity()
-def start_gravity_o():
-    global g
-    global new_letter
-    if(new_letter == "O"):
-        g = 1
-        gravity()
-def start_gravity_p():
-    global g
-    global new_letter
-    if(new_letter == "P"):
-        g = 1
-        gravity()
-def start_gravity_q():
-    global g
-    global new_letter
-    if(new_letter == "Q"):
-        g = 1
-        gravity()
-def start_gravity_r():
-    global g
-    global new_letter
-    if(new_letter == "R"):
-        g = 1
-        gravity()
-def start_gravity_s():
-    global g
-    global new_letter
-    if(new_letter == "S"):
-        g = 1
-        gravity()
-def start_gravity_t():
-    global g
-    global new_letter
-    if(new_letter == "T"):
-        g = 1
-        gravity()
-def start_gravity_u():
-    global g
-    global new_letter
-    if(new_letter == "U"):
-        g = 1
-        gravity()
-def start_gravity_v():
-    global g
-    global new_letter
-    if(new_letter == "V"):
-        g = 1
-        gravity()
-def start_gravity_w():
-    global g
-    global new_letter
-    if(new_letter == "W"):
-        g = 1
-        gravity()
-def start_gravity_x():
-    global g
-    global new_letter
-    if(new_letter == "X"):
-        g = 1
-        gravity()
-def start_gravity_y():
-    global g
-    global new_letter
-    if(new_letter == "Y"):
-        g = 1
-        gravity()
-def start_gravity_z():
-    global g
-    global new_letter
-    if(new_letter == "Z"):
-        g = 1
-        gravity()
-length = 25
-length -= 1
-new_letter = letters.pop(rndm.randint(0, length))
-def draw_apple(active_apple):
-    active_apple.shape(apple_image)
-    wn.update()
-    draw_letter()
-def draw_letter():
-    global new_letter
-    letter.goto((apple.xcor() - 23), (apple.ycor() - 47))
-    letter.write(new_letter, font=("Arial", 54, "bold"))
-g = 0
-ground = -180
-def gravity():
-    global g
-    if(g == 1):
-        letter.clear()
-        while(g == 1):
-            if(apple.ycor() >= -180):
-                apple.sety(apple.ycor() - 7)
-            if(apple.ycor() <= ground):
-                g = 0
-                move_apple(apple)
+def draw_apple(active_apple, letter):
+  active_apple.shape(apple_image)
+  active_apple.showturtle()
+  draw_letter(active_apple, letter)
+  wn.update()
+def drop_apple(letter):
+  wn.tracer(True)
+  index = current_letters.index(letter)
+  current_letters.pop(index)
 
-#TODO Create a function that takes a turtle as its parameter and gives that turtle (apple)
-def move_apple(trtl):
-    global new_letter
-    global length
-    new_apple_x = rndm.randint(-180, 150)
-    new_apple_y = rndm.randint(-50, 180)
-    apple.goto(new_apple_x, new_apple_y)
-    new_letter = letters.pop(rndm.randint(0, length))
-    length -= 1
-    draw_letter()
+  active_apple = apple_list.pop(index)
 
-draw_apple(apple)
+  active_apple.goto(active_apple.xcor(), ground_height)
+  active_apple.clear()
+  active_apple.hideturtle()
+  wn.tracer(False)
+  reset_apple(active_apple)
+  apple_list.append(active_apple)
+def draw_letter(active_apple, letter):
+  active_apple.color("white")
+  remember_position = active_apple.position()
+  active_apple.setpos(active_apple.xcor() + apple_letter_x_offset,active_apple.ycor() + apple_letter_y_offset)
+  active_apple.write(letter, font=("Arial", 40, "bold"))
+  active_apple.setpos(remember_position)
+for i in range(number_of_apples):
+    active_apple = trtl.Turtle(shape = apple_image)
+    active_apple.penup()
+    reset_apple(active_apple)
+    apple_list.append(active_apple)
+def check_letter_a():
+  if ("A" in current_letters):
+    drop_apple("A")
+
+def check_letter_b():
+  if ("B" in current_letters):
+    drop_apple("B")
+
+def check_letter_c():
+  if ("C" in current_letters):
+    drop_apple("C")
+
+def check_letter_d():
+  if ("D" in current_letters):
+    drop_apple("D")
+
+def check_letter_e():
+  if ("E" in current_letters):
+    drop_apple("E")
+
+def check_letter_f():
+  if ("F" in current_letters):
+    drop_apple("F")
+
+def check_letter_g():
+  if ("G" in current_letters):
+    drop_apple("G")
+
+def check_letter_h():
+  if ("H" in current_letters):
+    drop_apple("H")
+
+def check_letter_i():
+  if ("I" in current_letters):
+    drop_apple("I")
+
+def check_letter_j():
+  if ("J" in current_letters):
+    drop_apple("J")
+
+def check_letter_k():
+  if ("K" in current_letters):
+    drop_apple("K")
+
+def check_letter_l():
+  if ("L" in current_letters):
+    drop_apple("L")
+
+def check_letter_m():
+  if ("M" in current_letters):
+    drop_apple("M")
+
+def check_letter_n():
+  if ("N" in current_letters):
+    drop_apple("N")
+
+def check_letter_o():
+  if ("O" in current_letters):
+    drop_apple("O")
+
+def check_letter_p():
+  if ("P" in current_letters):
+    drop_apple("P")
+
+def check_letter_q():
+  if ("Q" in current_letters):
+    drop_apple("Q")
+
+def check_letter_r():
+  if ("R" in current_letters):
+    drop_apple("R")
+
+def check_letter_s():
+  if ("S" in current_letters):
+    drop_apple("S")
+
+def check_letter_t():
+  if ("T" in current_letters):
+    drop_apple("T")
+
+def check_letter_u():
+  if ("U" in current_letters):
+    drop_apple("U")
+
+def check_letter_v():
+  if ("V" in current_letters):
+    drop_apple("V")
+
+def check_letter_w():
+  if ("W" in current_letters):
+    drop_apple("W")
+
+def check_letter_x():
+  if ("X" in current_letters):
+    drop_apple("X")
+
+def check_letter_y():
+  if ("Y" in current_letters):
+    drop_apple("Y")
+    
+def check_letter_z():
+  if ("Z" in current_letters):
+    drop_apple("Z")
+
+# draw_apple(apple, "A")
+wn.onkeypress(check_letter_a, "a")
+wn.onkeypress(check_letter_b, "b")
+wn.onkeypress(check_letter_c, "c")
+wn.onkeypress(check_letter_d, "d")
+wn.onkeypress(check_letter_e, "e")
+wn.onkeypress(check_letter_f, "f")
+wn.onkeypress(check_letter_g, "g")
+wn.onkeypress(check_letter_h, "h")
+wn.onkeypress(check_letter_i, "i")
+wn.onkeypress(check_letter_j, "j")
+wn.onkeypress(check_letter_k, "k")
+wn.onkeypress(check_letter_l, "l")
+wn.onkeypress(check_letter_m, "m")
+wn.onkeypress(check_letter_n, "n")
+wn.onkeypress(check_letter_o, "o")
+wn.onkeypress(check_letter_p, "p")
+wn.onkeypress(check_letter_q, "q")
+wn.onkeypress(check_letter_r, "r")
+wn.onkeypress(check_letter_s, "s")
+wn.onkeypress(check_letter_t, "t")
+wn.onkeypress(check_letter_u, "u")
+wn.onkeypress(check_letter_v, "v")
+wn.onkeypress(check_letter_w, "w")
+wn.onkeypress(check_letter_x, "x")
+wn.onkeypress(check_letter_y, "y")
+wn.onkeypress(check_letter_z, "z")
 
 wn.listen()
-wn.onkeypress(start_gravity_a, "a")
-wn.onkeypress(start_gravity_b, "b")
-wn.onkeypress(start_gravity_c, "c")
-wn.onkeypress(start_gravity_d, "d")
-wn.onkeypress(start_gravity_e, "e")
-wn.onkeypress(start_gravity_f, "f")
-wn.onkeypress(start_gravity_g, "g")
-wn.onkeypress(start_gravity_h, "h")
-wn.onkeypress(start_gravity_i, "i")
-wn.onkeypress(start_gravity_j, "j")
-wn.onkeypress(start_gravity_k, "k")
-wn.onkeypress(start_gravity_l, "l")
-wn.onkeypress(start_gravity_m, "m")
-wn.onkeypress(start_gravity_n, "n")
-wn.onkeypress(start_gravity_o, "o")
-wn.onkeypress(start_gravity_p, "p")
-wn.onkeypress(start_gravity_q, "q")
-wn.onkeypress(start_gravity_r, "r")
-wn.onkeypress(start_gravity_s, "s")
-wn.onkeypress(start_gravity_t, "t")
-wn.onkeypress(start_gravity_u, "u")
-wn.onkeypress(start_gravity_v, "v")
-wn.onkeypress(start_gravity_w, "w")
-wn.onkeypress(start_gravity_x, "x")
-wn.onkeypress(start_gravity_y, "y")
-wn.onkeypress(start_gravity_z, "z")
-
-#TODO Create a function that takes a turtle (apple) and its corresponding letter from the letter
-# list and draws that letter on that turtle (apple)
-
-#TODO Create a function that takes a turtle (apple) and its corresponding letter from the letter
-# list and set that turtle to be shaped by the image file, call the letter drawing function,
-# and update the Screen
-
-#TODO Iterate over the numbers from 0 to the number of apples, creating that many turtles
-# calling your function that resets the apples by giving them a new random location
-# add the new apples to a list of apples to be used in the rest of the program.
-# The loop below executes the correct number of times by using the range() function
-# to create a list of numbers to iterate over.
-'''for i in range(0, number_of_apples):'''
-  #Your code here
-
-#TODO Create a function that takes a letter as its parameter, uses that letter to retrieve the
-# corresponding turtle (apple) and causes both to drop from the tree simultaneously. Once the 
-# apple and letter have dropped, call the apple reseting function.
-
-#TODO define a function per letter that you will use in your program. Each function should check
-# to see if the given letter is in the list of letters; if it is, it should drop the corresponding
-# apple.
-
-#TODO use the onkeypress method of wn to correlate the functions you defined above with each
-# of the letters that the user might type.
-# onkeypress requires that you name one function that must take
-# no arguments to be called when the specified key is pressed.
-
 wn.mainloop()
